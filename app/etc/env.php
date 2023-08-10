81a276acebc81c9c69529e4aaa883b7f3e7a56ec
Faith Kovi <55793103+FaithKovi@users.noreply.github.com>
2023-08-10 15:40:16 +0100

    Update env.php

--- app/etc/env.php
+++ app/etc/env.php
@@ -30,10 +30,10 @@ return [
         'table_prefix' => '',
         'connection' => [
             'default' => [
-                'host' => 'database_hostname',
-                'dbname' => 'database_name',
-                'username' => 'database_username',
-                'password' => 'database_password',
+                'host' => 'db-mysql-nyc1-71251-do-user-2224784-0.b.db.ondigitalocean.com:25060',
+                'dbname' => 'defaultdb',
+                'username' => 'doadmin',
+                'password' => 'AVNS_0Z31DfqXYzy-Sa89msf',
                 'model' => 'mysql4',
                 'engine' => 'innodb',
                 'initStatements' => 'SET NAMES utf8;',
